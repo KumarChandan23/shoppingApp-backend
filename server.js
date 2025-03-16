659331a271ae53d60aefef8f38c587d48100b3dd
chandan kumar <ck368966@gmail.com>
2025-03-16 18:36:59 +0530

    addedd cookie parser to server.js file

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ const { connectDB } = require("./config/db");
 const { PORT } = require("./config/index.JS");
 const userRouter = require("./routes/user.routes.js")
 const cors = require("cors");
+const cookieParser = require("cookie-parser")
+
+
 const app = express();
 
 connectDB();
 app.use(cors());
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/v1/users", userRouter)
 app.listen(PORT, (error) => {
